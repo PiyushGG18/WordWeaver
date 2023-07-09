a5cf3c445f2362b7094701aad6f7e3017ed5f883
Piyush <gawadepiyush007@gmail.com>
2023-07-09 16:47:47 +0530

    Regitration error fixed

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -7,21 +7,67 @@ function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error,setError] = useState(false);
+  const [error, setError] = useState(false);
+  const [warning, setWarning] = useState("Something went wrong");
+
+  function handleError(msg) {
+    if (msg.email == null && msg.username == null) {
+      switch (msg) {
+        case "User validation failed: username: Path `username` is required., email: Path `email` is required.":
+          setWarning("Please enter username and email address");
+          break;
+        case "User validation failed: email: Path `email` is required.":
+          setWarning("Please enter email");
+          break;
+        case "User validation failed: username: Path `username` is required.":
+          setWarning("Please enter username");
+          break;
+        default:
+          break;
+      }
+    } else {
+      if (msg.email == null) {
+        setWarning("Use another username");
+      } else {
+        setWarning("Use another email");
+      }
+    }
+  }
+
+  async function complete() {
+    try {
+      const res = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+    } catch (error) {
+      let msg;
+      error.response.data.message == null
+        ? (msg = error.response.data.keyPattern)
+        : (msg = error.response.data.message);
+      handleError(msg);
+      setError(true);
+    }
+  }
+  async function throwE() {
+    setWarning("Please enter password");
+    setError(true);
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios
-        .post("/auth/register", {
-          username,
-          email,
-          password,
-        })
-        res.data && window.location.replace("/login");
-      } catch (error) {
-        setError(true);
+      password === "" ? throwE() : complete();
+    } catch (error) {
+      let msg;
+      error.response.data.message == null
+        ? (msg = error.response.data.keyValue)
+        : (msg = error.response.data.message);
+      handleError(msg);
+      setError(true);
     }
   };
 
@@ -65,7 +111,9 @@ function Register() {
           Login
         </Link>
       </button>
-      {error && <span style={{color: "red", marginTop:"10px"}}>Something went wrong</span>}
+      {error && (
+        <span style={{ color: "red", marginTop: "10px" }}>{warning}</span>
+      )}
     </div>
   );
 }
